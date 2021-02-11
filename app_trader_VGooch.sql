af6808f48591cbd84dcd722ea6046ed0e33aaa1d
VGooch <valencia.gooch@gmail.com>
2021-02-11 11:45:53 -0600

    Initial Data Setup

---
+++ app_trader_VGooch.sql
@@ -0,0 +1,16 @@
+SELECT COUNT(*)
+FROM app_store_apps;
+--
+SELECT COUNT(*)
+FROM play_store_apps
+--
+SELECT *
+FROM app_store_apps
+LIMIT 5;
+--
+SELECT *
+FROM play_store_apps
+LIMIT 5;
+--
+
+-- DELIVERABLES
